d7f4b7768a477e2c7b34f2d00e615d4b8c94a38d
Anwesh43 <anweshthecool0@gmail.com>
2018-08-04 18:51:03 +0530

    adding RALNode class

--- RotateAboveLineStage.ts
+++ RotateAboveLineStage.ts
@@ -95,3 +95,44 @@ const drawRALNode = (context, i : number, scale : number) => {
     }
     context.restore()
 }
+
+class RALNode {
+
+    private next : RALNode
+    private prev : RALNode
+    private state : State = new State()
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new RALNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawRALNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : RALNode {
+        var curr = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return this
+        }
+        cb()
+        return this
+    }
+}
